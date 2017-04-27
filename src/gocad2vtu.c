506561782c991b26d8493d0322ee975f8d836643
homnath <hngharti@gmail.com>
2017-04-27 18:30:51 -0400

    - added gocad2vtu and xyz2jou

---
+++ src/gocad2vtu.c
@@ -0,0 +1,255 @@
+/*-----------------------------------------------------------------------------
+PURPOSE:
+  This program converts the GOCAD ASCII file (3-noded triangular meshes)
+  to VTK XML .vtu binary file (unstructured mesh file) which can be 
+  visualized/processed in ParaView or VTK
+DEVELOPER: 
+	Hom Nath Ghartii
+  formerly at Institute of Engineering, Tribhuvan University, Nepal
+  formerly at NORSAR, Norway
+	Department of Geosciences, Princeton University, USA
+DEPENDENCY:
+	stringmanip.c
+COMPILE:
+  - in parent folder, type:
+  make
+  OR
+  - in src/ folder, type
+	gcc gocad2vtu.c -o gocad2vtu
+USAGE:
+	./bin/gocad2vtu <inputfile> [OPTIONS]
+	Example: ./bin/gocad2vtu ./input/gocad2vtu_example.ts
+OPTIONS:
+	-fac=value
+		Use this option to multiply the coordinates by certain factor, this is 
+		helpful for unit conversion, e.g. for m to km use 0.001, for km to m 
+    use 1000, example: gocad2vtu T2_horizon.ts -fac=0.001
+NOTES:
+	Output .vtu file is binary, therefore endianness of the processor
+  architechture is important.
+	This program automatically identify the endianness and write the output
+  accordingly. Hence if you run and process/visualize .vtu file in the
+  architecture with different endianness there may be an error. 
+HISTORY: 
+	Hom Nath Gharti, NORSAR
+	Mar 18,2009;Mar 12,2009 (Princeton University)
+FEEDBACK:
+	hngharti_AT_gmail_DOT_com
+-----------------------------------------------------------------------------*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+#include "stringmanip.c"
+
+#define maxline 150 /* Max number of characters in a line */
+#define maxenod 3 /* Max number of nodes per element */
+#define maxvar 4 /* Max number of variable sets be plotted in ParaView file */
+#define LE 0 /* Little Endian */
+#define BE 1 /* Big Endian */
+
+int main(int argc,char **argv)
+{
+	int i,j,etype,nnod,nelmt; 
+	int endian,dumi,enod[maxenod];
+	int ndim,nenod,nvar;
+	int bytes[maxvar],off[maxvar],temp;
+	float tempx,tempy,tempz,fac;
+	char byte_order[12],buffer[maxline],dumc[10],fonly[62],outfname[62];
+	
+	FILE *inf,*outf0,*outf1; /* ,*outf_vtk; */
+	
+	ndim = 3; 	/* Number of dimensions */
+	nenod = 3; 	/* Number of nodes per element */
+	nvar = 4;
+	etype = 5; /* VTK triangle */	
+
+	printf("Processing...\n");
+		
+	fac=1.0;	
+		
+	/* Open input file */
+	if(argc<2){
+		fprintf(stderr,"ERROR: input file not entered!\n");
+		exit(-1);
+	}
+	inf=fopen(argv[1],"rb");
+	if(inf==NULL){
+		fprintf(stderr,"ERROR: file \"%s\" not found!",argv[1]);
+		exit(-1);
+	}
+	
+	/* Scan options */
+	if(argc>2){
+		for(i=2;i<argc;i++){
+			if(matchfirstword(argv[i],"-fac")){
+				getvalue(argv[i],"fac","f",(int *)&fac);
+			}
+			else{
+				printf("ERROR: unrecognized option \"%s\"",argv[i]);
+				exit(-1);
+			}
+		}
+	}
+	
+	/* Processor endianness */
+	endian=getEndian();
+	if(endian == LE){
+		strcpy(byte_order,"LittleEndian");
+	}
+	else if(endian == BE){
+		strcpy(byte_order,"BigEndian");
+	}
+	else{
+		printf("ERROR: illegal endianness!\n");
+		exit(-1);
+	}	
+	
+	printf("--------------------------------\n");
+	printf("Input file: %s\n",argv[1]);
+	printf("Coordinates multiplication factor: %f\n",fac);	
+	printf("--------------------------------\n");
+	
+	outf0=fopen("coord_temp","wb");
+	outf1=fopen("connect_temp","wb");
+	nnod=0; nelmt=0;
+	while(fgets(buffer,maxline,inf) != NULL){
+		if(matchfirstword(buffer,"VRTX") || matchfirstword(buffer,"PVRTX")){
+			nnod++;			
+			sscanf(buffer,"%s %d %f %f %f",dumc,&dumi,&tempx,&tempy,&tempz);
+			if(fac != 1.0){
+				tempx*=fac; tempy*=fac; tempz*=fac;
+			}
+			fwrite(&tempx,sizeof(float),1,outf0);
+			fwrite(&tempy,sizeof(float),1,outf0);
+			fwrite(&tempz,sizeof(float),1,outf0);
+		}
+		
+		if(matchfirstword(buffer,"TRGL")){
+			nelmt++;	
+			sscanf(buffer,"%s %d %d %d",dumc,&enod[0],&enod[1],&enod[2]);
+			fwrite(&enod[0],sizeof(int),1,outf1);
+			fwrite(&enod[1],sizeof(int),1,outf1);
+			fwrite(&enod[2],sizeof(int),1,outf1);
+		}
+	}
+	fclose(inf);
+	fclose(outf0);
+	fclose(outf1);
+	if(nnod<=0 || nelmt<=0){
+		printf("ERROR: zero entities [nodes=%d, elements=%d]!\n",nnod,nelmt);
+		exit(-1);
+	}
+	printf("Number of nodes: %d\n",nnod);
+	printf("Number of elements: %d\n",nelmt);
+	
+	printf("--------------------------------\n");
+	
+	bytes[0] = (ndim*nnod)*sizeof(float); 	/* Coordinates */
+	bytes[1] = (nenod*nelmt)*sizeof(int); 	/* Connectivity */
+	bytes[2] = (nelmt)*sizeof(int); 		/* Offsets */
+	bytes[3] = (nelmt)*sizeof(int); 		/* Types */
+	
+	off[0]=0; /* 1st offset */
+	for (i=0; i<nvar; i++){		
+		if(i<nvar-1)off[i+1]=off[i]+sizeof(int)+bytes[i];
+		bytes[i]=bytes[i]+sizeof(int);
+	}
+	
+	removeExtension(argv[1],fonly);	
+	sprintf(outfname,"%s_mesh.vtu",fonly);
+	
+	outf0=fopen(outfname,"wb");	
+	
+	fprintf(outf0,"<?xml version=\"1.0\"?>\n");
+	fprintf(outf0,"<VTKFile type=\"UnstructuredGrid\" version=\"0.1\" byte_order=\"%s\">\n",byte_order);
+	fprintf(outf0,"<UnstructuredGrid>\n");
+	fprintf(outf0,"<Piece NumberOfPoints=\"%d\" NumberOfCells=\"%d\">\n",nnod,nelmt);
+	fprintf(outf0,"<Points>\n");
+	fprintf(outf0,"<DataArray type=\"Float32\" NumberOfComponents=\"3\" format=\"appended\" offset=\"%d\"/>\n",off[0]);
+	fprintf(outf0,"</Points>\n");
+	fprintf(outf0,"<PointData>\n");
+	fprintf(outf0,"</PointData>\n");		
+	fprintf(outf0,"<Cells>\n");
+	fprintf(outf0,"<DataArray type=\"Int32\" Name=\"connectivity\" format=\"appended\" offset=\"%d\"/>\n",off[1]);
+	fprintf(outf0,"<DataArray type=\"Int32\" Name=\"offsets\" format=\"appended\" offset=\"%d\"/>\n",off[2]);
+	fprintf(outf0,"<DataArray type=\"Int32\" Name=\"types\" format=\"appended\" offset=\"%d\"/>\n",off[3]);
+	fprintf(outf0,"</Cells>\n");
+	fprintf(outf0,"<CellData>\n");
+	fprintf(outf0,"</CellData>\n");
+	fprintf(outf0,"</Piece>\n");
+	fprintf(outf0,"</UnstructuredGrid>\n");
+	fprintf(outf0,"<AppendedData encoding=\"raw\">\n");
+	fprintf(outf0,"_");
+	
+	inf=fopen("coord_temp","rb");
+	
+	/* outf_vtk=fopen("test.vtk","w");
+	fprintf(outf_vtk,"# vtk DataFile Version 2.0\n");
+	fprintf(outf_vtk,"Unstructured Grid Example\n");
+	fprintf(outf_vtk,"ASCII\n");
+	fprintf(outf_vtk,"DATASET UNSTRUCTURED_GRID\n");
+	fprintf(outf_vtk,"POINTS %d float\n",nnod);*/
+
+	/* Coordinates */
+	fwrite(&bytes[0],sizeof(int),1,outf0);
+	for(i=0;i<nnod;i++){
+		for(j=0;j<ndim;j++){
+			fread(&tempx,sizeof(float),1,inf);
+			fwrite(&tempx,sizeof(float),1,outf0);
+			/*fprintf(outf_vtk,"%.6f ",tempx);*/
+			/*if(j==ndim-1)fprintf(outf_vtk,"\n");*/
+		}
+	}
+	fclose(inf);
+	printf("Coordinates: SUCCESS\n");	
+	
+	inf=fopen("connect_temp","rb");
+	/*fprintf(outf_vtk,"CELLS %d %d\n",nelmt,nelmt*(nenod+1));*/
+	/* Connectivity */
+	fwrite(&bytes[1],sizeof(int),1,outf0);
+	for(i=0;i<nelmt;i++){
+		for(j=0;j<nenod;j++){
+			fread(&enod[j],sizeof(int),1,inf);
+			enod[j]--;
+			fwrite(&enod[j],sizeof(int),1,outf0);
+			/*printf("%d %d %d\n",enod[0],enod[1],enod[2]);*/
+			/*fprintf(outf_vtk,"4 %d %d %d\n",enod[0],enod[1],enod[2]);*/
+		}
+	}
+	fclose(inf);
+	remove("coord_temp");
+	remove("connect_temp");
+	
+	/* Offsets */
+	fwrite(&bytes[2],sizeof(int),1,outf0);
+	temp=0;
+	for(i=0;i<nelmt;i++){
+		temp+=3;
+		fwrite(&temp,sizeof(int),1,outf0);
+
+	}
+	printf("Offsets: SUCCESS\n");
+	
+	/* Types */
+	/*fprintf(outf_vtk,"CELL_TYPES %d\n",nelmt);*/
+	fwrite(&bytes[3],sizeof(int),1,outf0);
+	for(i=0;i<nelmt;i++){
+		fwrite(&etype,sizeof(int),1,outf0);
+		/*fprintf(outf_vtk,"%d\n",etype);*/
+		
+	}
+	/*fclose(outf_vtk);*/
+	printf("Types: SUCCESS\n");
+	
+	fprintf(outf0,"\n");
+	fprintf(outf0,"</AppendedData>\n");
+	fprintf(outf0,"</VTKFile>\n");
+	fclose(outf0);	
+	
+	printf("Status: SUCCESS\n");
+	printf("--------------------------------\n");	
+
+	return(0);
+}
---
+++ src/xyz2jou.f90
@@ -0,0 +1,273 @@
+!-----------------------------------------------------
+!PURPOSE:
+!  This program converts UTM or XYZ file to CUBIT journal file
+!COMPILE:
+!  - in parent folder, type:
+!  make
+!  OR
+!  - in src/ folder, type
+!  gfortran xyz2jou.f90 -o xyz2jou
+!RUN:
+!  ./bin/xyz2jou input_file [OPTIONS]
+!	 Example: ./bin/xyz2jou ./input/xyz2jou_example.utm
+!OPTIONS:
+!  -nx: Use this option if you know the number of points in a line along X axis
+!       . This will speed up the processing. For example, -nx=100. If not
+!       defined, nx is automatically determined. 
+!  -nskip: Use this option if you want to skip (downsample) certain number of i
+!       successive points. This will skip along both X and Y axes. For example,
+!       -nskip=2. [DEFAULT 0].
+!DEVELOPER:
+!  Hom Nath Gharti
+!  Department of Geosciences, Princeton University, USA
+!  hngharti_AT_gmail_DOT_edu
+!REVISION:
+!  March 02,2012
+!-----------------------------------------------------
+program xyz2jou
+implicit none
+integer,parameter :: kreal=selected_real_kind(12)
+character(len=180) :: cubit_fname,inp_fname,out_fname
+character(len=80) :: file_head,ext,strblk,path 
+integer :: ierr,ip1,ip2,narg,nx,nx0,npx
+integer :: np,npw ! total points, total points written
+integer :: nc,ncw ! total curves, total curves written
+integer :: ipskip,icskip
+integer :: i_arg,itmp,istat,nskip
+! tolerance for straight-line slope discrepancy
+real(kind=kreal),parameter :: eps=0.1_kreal
+real(kind=kreal) :: m0,m,x0,x,y0,y,z
+logical :: isnx0,setm0,islastc,islastp
+
+print*,'Running...'
+!----input and initialisation----
+narg=command_argument_count()
+if (narg <= 0) then
+  print*,'ERROR: no input file!'
+  stop
+endif
+! get input file name
+call get_command_argument(1, inp_fname)
+
+isnx0=.false.
+nskip=0
+if(narg>1)then
+  do i_arg=2,narg
+    call get_command_argument(i_arg, strblk)
+    ! read nx parameter
+    call look_int(strblk,'-nx=',itmp,istat)
+    if(istat.eq.0)then
+      nx0=itmp
+      isnx0=.true.
+      cycle
+    endif
+    
+    ! read nskip parameter
+    call look_int(strblk,'-nskip=',itmp,istat)
+    if(istat.eq.0)then
+      nskip=itmp
+      cycle
+    endif
+
+    ! unrecognized option
+    write(*,*)'ERROR: unrecognized option: ',trim(adjustl(strblk))
+    stop
+
+  enddo
+endif
+
+! display input info
+write(*,*)'------------------------------------------'
+write(*,*)'input file: ',trim(inp_fname)
+if(isnx0)then
+  write(*,*)'nx0 defined: YES'
+  write(*,*)'nx0: ',nx0
+else
+  write(*,*)'nx0 defined: NO'
+endif
+write(*,*)'nskip: ',nskip
+write(*,*)'------------------------------------------'
+
+open(10,file=inp_fname,status='old',action='read',iostat=ierr)
+if(ierr/=0)then
+  print*,'ERROR: cannot open file "',trim(inp_fname),'"!'
+  stop
+endif
+call parse_file(inp_fname,path,file_head,ext)
+out_fname=trim(file_head)//'.jou'
+open(20,file=out_fname,status='replace',action='write')
+
+read(10,*,iostat=ierr)x,y,z
+if(ierr/=0)then
+  print*,'ERROR: cannot read file!'
+  stop
+endif
+
+ip1=1; npx=1
+np=1; npw=1
+write(20,*)'reset'
+write(20,*)'create vertex ',x,y,z
+ipskip=0; icskip=0
+x0=x; y0=y
+
+nc=0; ncw=0;
+setm0=.true.
+islastc=.false.
+m0=100.0_kreal ! this value is redundant
+do
+  read(10,*,iostat=ierr)x,y,z
+  if(ierr/=0)exit
+  np=np+1
+  npx=npx+1
+  ipskip=ipskip+1
+  islastp=.false.
+  !write(20,*)'create vertex ',x,y,z
+  ! compute slope
+  m=(y-y0)/(x-x0)  
+  if(setm0)then
+    !write(20,*)'create vertex ',x,y,z
+    m0=m
+    setm0=.false.
+    npx=1
+  else  
+    if((isnx0.and.npx.eq.nx0) .or. (.not.isnx0.and.abs(m-m0).gt.eps))then
+      nx=npx ! point in each curve
+      !ip2=np-1
+      ip2=npw-1
+      nc=nc+1 ! number of curves
+      icskip=icskip+1
+
+      !if(nc.eq.1)allocate(lastpx(2*nx))
+      ! do not skip the first curve
+      if(nc.eq.1 .or. icskip.gt.nskip)then
+        write(20,*)'create curve spline vertex ',ip1,' to ',ip2
+        write(20,*)('delete vertex all')  
+        ncw=ncw+1
+        !if(ncw.ge.5)stop
+        icskip=0
+        islastc=.true.
+      endif
+      if((isnx0.or.nc>1).and.(nx0.ne.nx))then        
+        print*,'WARNING: number of Xpoints mismatch!'
+        print*,'nx0:',nx0,' nx:',nx
+        !stop
+      endif 
+      x0=x; y0=y
+      nx0=nx
+      setm0=.true.
+      ip1=ip2+1
+      if(nc.le.1)print*,'Total points in X direction:',nx0
+    endif
+    !write(20,*)'create vertex ',x,y,z    
+  endif
+  if((nc.eq.0 .or. icskip.gt.nskip-1) .and. ipskip.gt.nskip)then
+    npw=npw+1
+    write(20,*)'create vertex ',x,y,z
+    ipskip=0
+    islastp=.true.
+    islastc=.false.
+  endif
+  !if(np==30000)stop
+enddo
+close(10)
+
+!! do not skip last point
+!if(.not.islastp)then
+!  npw=npw+1
+!  write(20,*)'create vertex ',x,y,z
+!  ipskip=0
+!  islastp=.true.
+!endif
+
+! last curve
+if(.not.islastc)then
+  !ip2=np
+  ip2=npw
+  nc=nc+1 ! number of curves
+  ! do not skip last curve
+  write(20,*)'create curve spline vertex ',ip1,' to ',ip2
+  write(20,*)('delete vertex all')  
+  ncw=ncw+1
+endif
+if(nc*nx.ne.np)then
+  print*,'WARNING: total number of points mismatch!'
+endif
+print*,'Total points:',np
+print*,'Total points written:',npw
+print*,'Total points skipped:',np-npw
+print*,'Total curves:',nc
+print*,'Total curves written:',ncw
+print*,'Total curves skipped:',nc-ncw
+  
+write(20,*)'create surface skin curve all'
+ 
+write(20,*)('delete curve all')
+write(20,*)('compress all ')
+cubit_fname=trim(file_head)//'_surface.cub'
+write(20,*)'save as "',trim(cubit_fname),'" overwrite'
+close(20)
+print*,'Complete!'
+stop
+
+contains
+! parse file name and return path, file head, and extension
+subroutine parse_file(fname,path,head,ext)
+implicit none
+character(len=*),intent(in) :: fname
+character(len=*),intent(out) :: path,head,ext
+integer :: i,ipath,iext,slen
+
+slen=len(fname)
+
+! set default output
+path=""
+head=""
+ext=""
+
+if(len_trim(fname)==0)return
+
+! find dot position
+iext=slen+1
+do i=slen,1,-1
+  if(fname(i:i)==".")then
+    iext=i
+    exit
+  endif
+enddo
+
+! find slash position
+ipath=0
+do i=iext,1,-1
+  if(fname(i:i)=="/" .or. fname(i:i)=="\")then
+    ipath=i
+    exit
+  endif
+enddo
+
+! determine path, head, and extension
+head=fname(ipath+1:iext-1)
+path=fname(1:ipath)
+ext=fname(iext+1:slen)
+return
+end subroutine parse_file
+!==============================================================================
+
+subroutine look_int(strblk,vartag,ival,istat)
+implicit none
+character(len=*),intent(in) :: strblk,vartag
+integer,intent(out) :: ival,istat
+integer :: ios,ncstr,ncvar
+ncstr=len(trim(strblk))
+ncvar=len(trim(vartag))
+istat=-1
+if(vartag(1:ncvar) .eq. strblk(1:ncvar))then
+  ! variable found
+  read(strblk(ncvar+1:ncstr),*,iostat=ios)ival
+  if(ios.ne.0)return
+  istat=0
+endif
+return
+end subroutine look_int
+!==============================================================================
+
+end program xyz2jou
