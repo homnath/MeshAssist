3fe7e7eb11785fc3eaf3414ae4cbbc487a117592
homnath <hngharti@gmail.com>
2017-05-11 12:23:49 -0400

    - added write_vti.m

---
+++ src/write_vti.m
@@ -0,0 +1,53 @@
+% FILE
+%   write_vti.m
+% PURPOSE
+%   This function writes the VTI binary file for structured grid data, such as 
+%   finite difference data and tomography data. The VTI file can be visualized 
+%   in ParaView (http://www.paraview.org/).
+% INPUT
+%   fname   : output file name
+%   ox      : origin vector [ox oy oz]
+%   dh      : sampling interval vector [dx dy dz]
+%   nx      : grid number vector [nx ny nz]
+%   name    : output variable name
+% USE
+%   call this functions with appropriate variables.
+% AUTHORS
+%   Hom Nath Gharti, Princeton University
+% REVISION
+%   NOV 20,2008: Hom Nath Gharti
+%   - First created
+% NOTES
+%   - For a BigEndian architecture, replace "LittleEndian" with "BigEndian"
+%     in the line below.
+function write_vti(fname,ox,dh,nx,data,name)
+
+outf=fopen(fname,'w');
+fprintf(outf,'<?xml version="1.0"?>\n');
+fprintf(outf,'<VTKFile type="ImageData" version="0.1" byte_order="LittleEndian">\n');
+fprintf(outf,'<ImageData WholeExtent="0 %d 0 %d 0 %d" Origin="%.6f %.6f %.6f" Spacing="%.6f %.6f %.6f">\n'...
+    ,nx(1)-1,nx(2)-1,nx(3)-1,ox(1),ox(2),ox(3),dh(1),dh(2),dh(3));
+fprintf(outf,'<Piece Extent="0 %d 0 %d 0 %d">\n',nx(1)-1,nx(2)-1,nx(3)-1);
+fprintf(outf,'<PointData Scalars="%s">\n',name);
+
+offset=0;
+ncomp=1;
+byte_int=4;
+byte_float=4;
+nbyte=nx(1)*nx(2)*nx(3)*ncomp*byte_float;
+nbyte=nbyte+byte_int;
+
+fprintf(outf,'<DataArray type="Float32" Name="%s" format="appended" offset="%d" />\n',name,offset);
+fprintf(outf,'</PointData>\n');
+fprintf(outf,'<CellData>\n');
+fprintf(outf,'</CellData>\n');
+fprintf(outf,'</Piece>\n');
+fprintf(outf,'</ImageData>\n');
+fprintf(outf,'<AppendedData encoding="raw">\n');
+fprintf(outf,'_');
+fwrite(outf,nbyte,'int32');
+fwrite(outf,data,'float32');
+fprintf(outf,'\n');
+fprintf(outf,'</AppendedData>\n');
+fprintf(outf,'</VTKFile>\n');
+fclose(outf);
